7a0f05a97eb8a9dee57c5589ba58dfda168fdbe7
ikeaosman <ikhr0003@stud.kea.dk>
2023-05-13 12:40:03 +0200

    6

--- src/main/java/com/example/bilabonnement/repository/CustomerRepo.java
+++ src/main/java/com/example/bilabonnement/repository/CustomerRepo.java
@@ -2,7 +2,6 @@ package com.example.bilabonnement.repository;
 
 import com.example.bilabonnement.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
