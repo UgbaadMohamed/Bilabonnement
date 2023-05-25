244d3c6e49d6b2439d39247927b6f9a7c0a54650
ikeaosman <ikhr0003@stud.kea.dk>
2023-05-25 17:53:46 +0200

    class diagram

--- src/test/java/com/example/bilabonnement/service/CarServiceTest.java
+++ src/test/java/com/example/bilabonnement/service/CarServiceTest.java
@@ -17,35 +17,5 @@ class CarServiceTest {
 
     }
 
-    /*
 
-       public void convertPrice(Car car, String currency) {
-        if (currency.equals("eu")) {
-            car.setCar_price(car.getCar_price() * 0.1343);
-        }
-    }
-     */
-    /*
-
-        MyClass obj = new MyClass();
-
-        // Act
-        obj.myMethod();
-
-        // Assert
-        // Add assertions here to verify the side effects or changes caused by the method
-        // For example, you could check the state of an object after calling the method
-
-        // Assert that a specific attribute has been modified
-        assertEquals(expectedValue, obj.getAttribute());
-
-        // Assert that a specific method has been called
-        assertTrue(obj.isOtherMethodCalled());
-
-        // Assert that a specific condition is met
-        assertTrue(condition);
-    }
-}
-
-     */
 }
